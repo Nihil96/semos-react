8aa1400991b2cd38217bd74e2e2500545f7efb47
Nihil96 <kostadinoskihristijan@yahoo.com>
2025-10-06 21:56:18 +0200

    examples for lecture 1

---
+++ src/examples/lecture_1/components/classComponent/classComponent.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react"
+import { Button } from "../button/button"
+
+class MyClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h3>This is my first Class Component</h3>
+                <Button />
+            </div>
+        )
+    }
+}
+
+export default MyClassComponent
---
+++ src/examples/lecture_1/components/myFirstComponent.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./button/button"
+
+function MyFirstComponent(){
+    return(
+        <div>
+            <h1>My First Component</h1>
+            <Button />
+        </div>
+    )
+}
+
+export default MyFirstComponent
